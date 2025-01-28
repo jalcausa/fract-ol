453e99929d900262ee73ef560b0ddfe51ca73326
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2025-01-28 18:16:31 +0100

    header file

---
+++ include/fractol.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:14:12 by jalcausa          #+#    #+#             */
+/*   Updated: 2025/01/28 18:14:56 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <math.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdint.h>
+# include <string.h>
+# include <unistd.h>
+# include "../MLX42/include/MLX42/MLX42.h"
+//# include "../libft/libft.h"
+
+# ifndef WIDTH
+#  define WIDTH 1000
+# endif
+
+# ifndef HEIGHT
+#  define HEIGHT 1000
+# endif
+
+# ifndef MAX_ITERATIONS
+#  define MAX_ITERATIONS 100
+# endif
+
+#endif
